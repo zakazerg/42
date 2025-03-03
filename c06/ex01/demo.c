fe58232825c58164a83ade5bf594177b65835e1c
Zakaria Zerg <zzerg@made-f0Ar13s1.clusters.42paris.fr>
2025-03-03 05:04:54 +0100

    ex

--- c06/ex01/demo.c
+++
@@ -1,3 +0,0 @@
-	char *str;:
-
-	str = malloc(char *(26 * 2) - 2);
--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: zzerg <zzerg@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 23:27:13 by zzerg             #+#    #+#             */
-/*   Updated: 2025/02/28 05:16:51 by zzerg            ###   ########.fr       */
+/*   Updated: 2025/03/03 04:57:13 by zzerg            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,18 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
-		if (s1[i] < s2[i])
-			return (-1);
-		else if (s1[i] == s2[i])
-			return (0);
-		else
-			return (1);
 		i++;
 	}
+	return(s1[i] - s2[i]);
 }
 
-
-void ft_sort_int_tab(char tab[], int size)
+void ft_sort_int_tab(char **tab, int size)
 {
 	int i;
 	int n;
-	int tmp;
+	char* tmp;
 	int z;
 
 	z = 1;
@@ -53,11 +47,11 @@ void ft_sort_int_tab(char tab[], int size)
 		while (i <= size - 1)
 		{
 			n = i;
-			while ((ft_strcmp(tab[i], tab[n])) <= 0	&& n < size - 1)
+			while (ft_strcmp(tab[i], tab[n]) == 0 && n < size - 1)
 			{
 				n++;
 			}
-			if ((ft_strcmp(tab[i], tab[n]) > 0))
+			if (ft_strcmp(tab[i], tab[n]) > 0)
 			{
 				tmp = tab[i];
 				tab[i] = tab[n];
@@ -71,7 +65,17 @@ void ft_sort_int_tab(char tab[], int size)
 
 int main(int argc, char *argv[])
 {
-	ft_putstr(ft_sort_int_tab(argv, argc));
+	int i;
 
+	argc = argc - 1;
+	argv++;
+	ft_sort_int_tab(argv, argc);
+	i = 0;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
 	return (0);
 }	
